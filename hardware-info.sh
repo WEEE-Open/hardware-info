b6f03197c08a068fe6b9cb9c777eb0136a3f835d
Luca Savant <luca.mezzelfo@gmail.com>
2018-05-19 11:22:32 +0200

    Add files via upload
    This version is less generic but more effective

--- hardware-info.sh
+++ hardware-info.sh
@@ -1,36 +1,14 @@
-#! /bin/sh
-# Variables
-dependencies=( "grep" "lsblk" "lshw" "lspci" "lscpu" "smartctl")
+#! /bin/bash
 commands=("echo WEEE Open" "lshw" "lspci -v" "lscpu" "./WEEE_SmartCTL")
+#Checking root
+if (( $EUID != 0 )); then
+    echo "Please run me as root"
+   exit 1
+fi
 
-#Check dependencies
-for dep in ${dependencies[@]}
-do
-	echo "Testing $dep ..."
-    if ! [ -x "$(command -v $dep)" ]; then
-  		echo "Error: $dep is not installed. Do you want me to try to install it?"
-  		read answer
-  		if [ "$answer" != "${answer#[Yy]}" ] ;then
-			if (( $EUID != 0 )); then
-    			echo "Please run me as root"
-   				 exit 1
-			fi
-
-			#Installing missing dependencies
-			echo "Trying to install $dep"
-			apt-get install -y $dep
-
-			#Testing if dep is now installed
-			if ! [ -x "$(command -v $dep)" ]; then
-				echo "Installation failed :("
-				exit 3
-			fi
-		else
-    		echo No
-    		exit 2
-		fi
-	fi
-done
+# Installing only smartmontools
+echo "Installing smartmontools"
+apt-get --no-install-recommends install -y smartmontools
 
 # Creating output file
 echo "Sto creando il file di log..."
